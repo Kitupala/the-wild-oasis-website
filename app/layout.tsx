28b6e1791e1f36c46d8d6412b88551790a00f9ed
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-13 11:48:02 +0300

    Add Header and additional styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
+import Header from "./_components/Header";
 import "@/app/_styles/globals.css";
-
 import { Josefin_Sans } from "next/font/google";
 
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
-console.log(josefin);
 
 export const metadata = {
   title: {
@@ -25,14 +21,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+        className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
