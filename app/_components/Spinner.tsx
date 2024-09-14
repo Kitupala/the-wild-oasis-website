a1a71b32f440c4752b8532a8ba681be55ce20c51
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-15 00:03:26 +0300

    Edit component to show optional text

--- app/_components/Spinner.tsx
+++ app/_components/Spinner.tsx
@@ -1,5 +1,14 @@
-function Spinner() {
-  return <div className='spinner'></div>;
+interface SpinnerProps {
+  text?: string;
+}
+
+function Spinner({ text }: SpinnerProps) {
+  return (
+    <div className="grid items-center justify-center">
+      <div className="spinner"></div>
+      <p className="text-xl mt-2 text-primary-200">{text}</p>
+    </div>
+  );
 }
 
 export default Spinner;
