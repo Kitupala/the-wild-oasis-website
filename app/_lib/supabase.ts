d5c9a18cadd3e275be16dea9780458cd5add4145
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-14 19:13:36 +0300

    Create the Supabase client

---
+++ app/_lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create and export the Supabase client
+export const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_KEY!
+);
