567b006cde85c6f0befe40052367be2e73c57749
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-17 14:07:32 +0300

    Add cabin filter feature

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -2,16 +2,30 @@
 
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
+import { Cabin, CabinFilter } from "@/app/types/cabin";
 
-async function CabinList() {
+interface CabinListProps {
+  filter: CabinFilter;
+}
+
+async function CabinList({ filter }: CabinListProps) {
   // noStore();
-  const cabins = await getCabins();
+  const cabins: Cabin[] = await getCabins();
 
   if (!cabins.length) return null;
 
+  const filters: Record<CabinFilter, (cabin: Cabin) => boolean> = {
+    all: () => true,
+    small: (cabin: Cabin) => cabin.maxCapacity <= 3,
+    medium: (cabin: Cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7,
+    large: (cabin: Cabin) => cabin.maxCapacity >= 8,
+  };
+
+  const displayedCabins = cabins.filter(filters[filter]);
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
