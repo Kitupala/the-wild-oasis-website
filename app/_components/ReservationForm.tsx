5877858662a0b91952d87497a81a14b1896e9fc3
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-23 11:03:40 +0300

    Set the user session

--- app/_components/ReservationForm.tsx
+++ app/_components/ReservationForm.tsx
@@ -2,12 +2,14 @@
 
 import { Cabin } from "@/app/types/cabin";
 import { useReservation } from "./ReservationContext";
+import { User } from "next-auth";
 
 interface ReservationFormProps {
   cabin: Cabin;
+  user: User;
 }
 
-function ReservationForm({ cabin }: ReservationFormProps) {
+function ReservationForm({ cabin, user }: ReservationFormProps) {
   const { range } = useReservation();
   const { maxCapacity } = cabin;
 
@@ -15,20 +17,17 @@ function ReservationForm({ cabin }: ReservationFormProps) {
     <div className="scale-[1.01]">
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
-        <p>
-          from {String(range?.from)} to {String(range?.to)}
-        </p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <img
             // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
+            referrerPolicy="no-referrer"
+            className="h-8 rounded-full"
+            src={user.image || ""}
+            alt={user.name || "Anonymous"}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
