e4483ac69c36e1690adffea75ca2f9d94bc85148
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-23 11:48:02 +0300

    Config to use middleware

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,13 +1,25 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import { NextRequest } from "next/server";
+import NextAuth, { Session } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 const authConfig = {
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
     }),
   ],
+  callbacks: {
+    authorized({
+      auth,
+      request,
+    }: {
+      auth: Session | null;
+      request: NextRequest;
+    }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
