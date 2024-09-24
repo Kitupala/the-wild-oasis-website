faa373623c7fb0fbefb9676f699b481bba01164d
Kitupala <kimmo.tuupanen@gmail.com>
2024-09-24 12:39:34 +0300

    Add types

---
+++ app/types/booking.d.ts
@@ -0,0 +1,17 @@
+interface CabinSummary {
+  name: string;
+  image: string;
+}
+
+export interface Booking {
+  id: number;
+  created_at: string;
+  startDate: string;
+  endDate: string;
+  numNights: number;
+  numGuests: number;
+  totalPrice: number;
+  guestId: number;
+  cabinId: number;
+  cabins: CabinSummary;
+}
